c0c76291f12927aaf95b7ab11f4e36a58adb0a29
Stan <nwankwo.stanislaus@gmail.com>
2020-11-24 19:30:20 +0100

    add edit home link

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
     <header id="header" class="fixed-top">
       <div class="container d-flex align-items-center">
         <!-- Uncomment below if you prefer to use an image logo -->
-        <a href="index.html" class="logo mr-auto"
+        <a href="index.php" class="logo mr-auto"
           ><img src="assets/img/cabolog-logo.png" alt="" class="img-fluid"
         /></a>
 

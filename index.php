ead6333daa4832a257bc5f2c3c433e6c5c26fe0e
Stan <nwankwo.stanislaus@gmail.com>
2020-11-24 19:43:34 +0100

    add edit home link 2

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
 
         <nav class="nav-menu d-none d-lg-block">
           <ul>
-            <li class="active"><a href="index.html">Home</a></li>
+            <li class="active"><a href="index.php">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#services">Services</a></li>
             <li><a href="#team">Team</a></li>
